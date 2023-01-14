65679dc81a65cd2088c63f37f37ec0556b188e26
Nikita <405663260nikita@gmail.com>
2023-01-14 17:50:58 +0300

    add function get_data, get_symbol_rating, change all another function

--- utils.py
+++ utils.py
@@ -1,17 +1,27 @@
 import sqlite3
 import json
 
-DB_PATH = './netflix.db'
+
+def get_data(query: str):
+    """
+    Обращается к базе данных
+    :param query: SQL запрос
+    :return: результат запроса
+    """
+    DB_PATH = './netflix.db'
+
+    connection = sqlite3.connect(DB_PATH)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    return cursor.fetchall()
 
 
-def title_search(title):
+def title_search(title: str) -> dict:
     """
     Ищет последний фильм по названию
     :param title: название фильма
     :return: информация о фильме
     """
-    connection = sqlite3.connect(DB_PATH)
-    cursor = connection.cursor()
     query = f"""
                 SELECT title, country, release_year, listed_in, description
                 FROM netflix
@@ -19,8 +29,7 @@ def title_search(title):
                 ORDER BY release_year DESC
                 LIMIT 1
             """
-    cursor.execute(query)
-    result = cursor.fetchall()
+    result = get_data(query)
     film = {
         "title": result[0][0],
         "country": result[0][1],
@@ -32,15 +41,13 @@ def title_search(title):
     return film
 
 
-def year_to_year_search(year_1, year_2):
+def year_to_year_search(year_1: int, year_2: int) -> list:
     """
     Ищет фильмы, выпущенные с year_1 года по year_2 года
     :param year_1: год от
     :param year_2: год до
     :return: список фильмов
     """
-    connection = sqlite3.connect(DB_PATH)
-    cursor = connection.cursor()
     query = f"""
                 SELECT title, release_year
                 FROM netflix
@@ -48,43 +55,37 @@ def year_to_year_search(year_1, year_2):
                 ORDER BY release_year
                 LIMIT 100
             """
-    cursor.execute(query)
-    result = cursor.fetchall()
-
-    movies = []
-    for movie in result:
-        one_movie = {
-            "title": movie[0],
-            "release_year": movie[1],
-        }
-        movies.append(one_movie)
+    result = get_data(query)
+    movies = [{"title": movie[0], "release_year": movie[1]} for movie in result]
 
     return movies
 
 
-def rating_movies(rating):
+def get_symbol_rating(rating: str) -> list:
+    if rating == 'children':
+        return ["'G'"]
+    elif rating == 'family':
+        return ["'G'", "'PG'", "'PG-13'"]
+    elif rating == 'adult':
+        return ["'R'", "'NC-17'"]
+    else:
+        return []
+
+
+def rating_movies(rating: str) -> list:
     """
     Ищет фильмы согласно рейтингу
     :param rating: нужный рейтинг
     :return: список фильмов
     """
-    symbol_rating = []
-    if rating == 'children':
-        symbol_rating = ["'G'"]
-    elif rating == 'family':
-        symbol_rating = ["'G'", "'PG'", "'PG-13'"]
-    elif rating == 'adult':
-        symbol_rating = ["'R'", "'NC-17'"]
+    symbol_rating = get_symbol_rating(rating)
 
-    connection = sqlite3.connect(DB_PATH)
-    cursor = connection.cursor()
     query = f"""
             SELECT title, rating, description
             FROM netflix
             WHERE rating IN ({', '.join(symbol_rating)})
             """
-    cursor.execute(query)
-    result = cursor.fetchall()
+    result = get_data(query)
 
     movies = []
     for movie in result:
@@ -98,14 +99,12 @@ def rating_movies(rating):
     return movies
 
 
-def genre_search(genre):
+def genre_search(genre: str):
     """
     Ищет фильмы по нужному жанру
     :param genre: жанр фильма
     :return: список фильмов
     """
-    connection = sqlite3.connect(DB_PATH)
-    cursor = connection.cursor()
     query = f"""
             SELECT title, description
             FROM netflix
@@ -113,39 +112,28 @@ def genre_search(genre):
             ORDER BY release_year DESC
             LIMIT 10
             """
-    cursor.execute(query)
-    result = cursor.fetchall()
-
-    movies = []
-    for movie in result:
-        one_movie = {
-            "title": movie[0],
-            "description": movie[1],
-        }
-        movies.append(one_movie)
+    result = get_data(query)
+    movies = [{"title": movie[0], "description": movie[1]} for movie in result]
 
     return json.dumps(movies)
 
 
-def find_actors(name_1, name_2):
+def find_actors(name_1: str, name_2: str):
     """
     Находит актеров, которые играли с актерами name_1 и name_2 более двух раз
     :param name_1: имя первого актера
     :param name_2: имя второго актера
     :return: список актеров
     """
-    connection = sqlite3.connect(DB_PATH)
-    cursor = connection.cursor()
     query = f"""
                 SELECT netflix.cast
                 FROM netflix
                 WHERE netflix.cast LIKE '%{name_1}%'
                 AND netflix.cast LIKE '%{name_2}%'
                 """
-    cursor.execute(query)
-    result = cursor.fetchall()
+    result = get_data(query)
 
-    # список всех актеров
+    # список всех актеров с повторениями
     all_actors = []
     for film in result:
         all_actors.extend(film[0].split(', '))
@@ -167,8 +155,6 @@ def find_for_type_year_genre(type_: str, year: int, genre: str):
     :param genre: жанр
     :return: список картин
     """
-    connection = sqlite3.connect(DB_PATH)
-    cursor = connection.cursor()
     query = f"""
                     SELECT title, description
                     FROM netflix
@@ -176,15 +162,7 @@ def find_for_type_year_genre(type_: str, year: int, genre: str):
                     AND release_year = {year}
                     AND listed_in LIKE '%{genre.capitalize()}%'
                     """
-    cursor.execute(query)
-    result = cursor.fetchall()
-
-    movies = []
-    for movie in result:
-        one_movie = {
-            "title": movie[0],
-            "description": movie[1],
-        }
-        movies.append(one_movie)
+    result = get_data(query)
+    movies = [{"title": movie[0], "description": movie[1]} for movie in result]
 
     return movies
